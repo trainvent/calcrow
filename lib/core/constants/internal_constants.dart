671e71d9f456f1aba83e0f4ad5133381ad589c6e
lmarquar <leon.marquardt@gmx.de>
2026-03-25 20:00:38 +0100

    updated adsid

--- lib/core/constants/internal_constants.dart
+++ lib/core/constants/internal_constants.dart
@@ -1,11 +1,11 @@
 class IConst {
   static const String appName = 'Calcrow';
   static const String adMobAndroidAppId =
-      'ca-app-pub-5296065079333841~7731988340';
+      'Calcrowca-app-pub-6799570171188466~4514817548';
   static const String adMobAndroidInterstitialId =
-      'ca-app-pub-5296065079333841/5488463809';
+      'ca-app-pub-6799570171188466/5226307567';
   static const String adMobAndroidBannerId =
-      'ca-app-pub-5296065079333841/3488463809';
+      'ca-app-pub-6799570171188466/4516561820';
   static const String revenueCatTestAPIKey = 'test_zFrDCqEavHplIKHUXrRXIoxDYff';
   static const String revenueCatGoogleAPIKey =
       'goog_xKUhAmZZmZcbJhlLNFqYBpbbQIK';
